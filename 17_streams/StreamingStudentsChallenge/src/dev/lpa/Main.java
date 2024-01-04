cef4d51da7665e9de5fca8f36ff0ce9be08a3140
Jonathan Butler <xxbutler86xx@gmail.com>
2024-01-04 05:50:59 -0500

    ASFSAD

--- 17_streams/StreamingStudentsChallenge/src/dev/lpa/Main.java
+++ 17_streams/StreamingStudentsChallenge/src/dev/lpa/Main.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -16,9 +18,81 @@ public class Main {
 //        tim.watchLecture("PYMC", 7, 7, 2020);
 //        System.out.println(tim);
 
-        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(10)
+//        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+//                .limit(10)
+//                .forEach(System.out::println);
+
+        Student[] students = new Student[1000];
+        Arrays.setAll(students, (i) -> Student.getRandomStudent(jmc, pymc));
+
+        var maleStudents = Arrays.stream(students)
+                        .filter(s-> s.getGender().equals("M"));
+
+        System.out.println("# of male students " + maleStudents.count());
+
+        for (String gender : List.of("M", "F", "U")) {
+            var myStudents = Arrays.stream(students)
+                    .filter(s-> s.getGender().equals(gender));
+            System.out.println("# of " + gender + " students " + myStudents.count());
+        }
+
+        List<Predicate<Student>> list = List.of(
+                (s) -> s.getAge() < 30,
+                (Student s) -> s.getAge() >= 30 && s.getAge() < 60
+        );
+
+        long total = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            var myStudents = Arrays.stream(students)
+                    .filter(list.get(i));
+                    long cnt = myStudents.count();
+                    total += cnt;
+                    System.out.printf("# of students (%s = %d%n",
+                            i == 0 ? " < 30)" : ">= 30 & < 60)", cnt);
+        }
+
+        System.out.println("# of students >= 60 = " + (students.length - total));
+
+        var ageStream = Arrays.stream(students)
+                .mapToInt(Student::getAgeEnrolled);
+        System.out.println("Stats for Enrollment Age = " + ageStream.summaryStatistics());
+
+        var currentAgeStream = Arrays.stream(students)
+                .mapToInt(Student::getAge);
+        System.out.println("Stats for Current Age = " + currentAgeStream.summaryStatistics());
+
+        Arrays.stream(students)
+                .map(Student::getCountryCode)
+                .distinct()
+                .sorted()
+                .forEach(s-> System.out.print(s + " "));
+
+        System.out.println();
+
+        boolean longTerm = Arrays.stream(students)
+                .anyMatch(s-> (s.getAge() - s.getAgeEnrolled() >= 7) &&
+                        (s.getMonthsSinceActive() < 12));
+
+        System.out.println("longTerm students? " + longTerm);
+
+
+        long longTermCount = Arrays.stream(students)
+                .filter(s-> (s.getAge() - s.getAgeEnrolled() >= 7) &&
+                        (s.getMonthsSinceActive() < 12))
+                        .count();
+
+        System.out.println("longTerm students? " + longTermCount);
+
+        Arrays.stream(students)
+                .filter(s-> (s.getAge() - s.getAgeEnrolled() >= 7) &&
+                        (s.getMonthsSinceActive() < 12))
+                .filter(s -> !s.hasProgrammingExperience())
+                .limit(5)
                 .forEach(System.out::println);
 
-    }
+
+}
+
+
 }
