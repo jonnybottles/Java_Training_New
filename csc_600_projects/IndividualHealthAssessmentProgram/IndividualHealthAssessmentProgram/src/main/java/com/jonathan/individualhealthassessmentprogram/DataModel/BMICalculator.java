953f64db894e93a62cb364b039b93eb11a9fd809
Jonathan Butler <xxbutler86xx@gmail.com>
2024-02-04 07:47:12 -0500

    ASDFSDA

---
+++ csc_600_projects/IndividualHealthAssessmentProgram/IndividualHealthAssessmentProgram/src/main/java/com/jonathan/individualhealthassessmentprogram/DataModel/BMICalculator.java
@@ -0,0 +1,81 @@
+package com.jonathan.individualhealthassessmentprogram.DataModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BMICalculator {
+
+    private float weight;
+    private float height;
+    private float BMI;
+    private List<String> BMIStrings;
+
+    public BMICalculator() {
+        this.BMIStrings = new ArrayList<>(Arrays.asList(
+                "Seriously Underweight",
+                "Underweight",
+                "Normal Weight",
+                "Overweight",
+                "Obese"));
+
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public String getBMI() {
+        if (BMI == 0) {
+            calculateBMI();
+            if (BMI == 0) {
+                return "BMI not calculated. Set height and weight first.";
+            }
+        }
+        return String.format("%.2f", BMI);
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public String getBMICategory() {
+        int listIndex = -1;
+        if (BMI == 0) {
+            System.out.println("BMI was never calculated and is set to zero.");
+            return "Invalid BMI value";
+        } else if (BMI < 18) {
+            listIndex = 0;
+        } else if (BMI < 18.5) {
+            listIndex = 1;
+        } else if (BMI <= 24.9) {
+            listIndex = 2;
+        } else if (BMI <= 29.9) {
+            listIndex = 3;
+        } else {
+            listIndex = 4;
+        }
+
+        return BMIStrings.get(listIndex);
+    }
+
+    public void calculateBMI() {
+
+        if (height != 0 && weight != 0) {
+            BMI = weight / (height * height);
+        } else {
+            System.out.println("Height and weight must be set prior to calculating BMI.");
+        }
+
+
+    }
+
+}
---
+++ csc_600_projects/IndividualHealthAssessmentProgram/IndividualHealthAssessmentProgram/src/main/java/com/jonathan/individualhealthassessmentprogram/Main.java
@@ -0,0 +1,50 @@
+package com.jonathan.individualhealthassessmentprogram;
+
+import com.jonathan.individualhealthassessmentprogram.DataModel.BMICalculator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        testBMICalculator();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    private void testBMICalculator() {
+        List<float[]> testCases = new ArrayList<>();
+        // Add test cases as {weight, height} pairs
+        testCases.add(new float[]{45f, 1.75f}); // Seriously Underweight
+        testCases.add(new float[]{56.5f, 1.75f}); // Underweight
+        testCases.add(new float[]{68f, 1.75f}); // Normal Weight
+        testCases.add(new float[]{80f, 1.75f}); // Overweight
+        testCases.add(new float[]{100f, 1.75f}); // Obese
+
+        for (float[] testCase : testCases) {
+            BMICalculator calculator = new BMICalculator();
+            calculator.setHeight(testCase[1]);
+            calculator.setWeight(testCase[0]);
+            calculator.calculateBMI();
+            System.out.println("For weight " + testCase[0] + "kg and height " + testCase[1] + "m, BMI is: " +
+                    calculator.getBMI() + " BMI Category is: " + calculator.getBMICategory());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
