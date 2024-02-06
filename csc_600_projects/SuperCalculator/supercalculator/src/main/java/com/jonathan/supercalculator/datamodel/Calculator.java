332126a431960ef263bdfdd0206b9ce6303f2280
Jonathan Butler <xxbutler86xx@gmail.com>
2024-02-06 17:47:08 -0500

    ASFSD

--- csc_600_projects/SuperCalculator/supercalculator/src/main/java/com/jonathan/supercalculator/datamodel/Calculator.java
+++ csc_600_projects/SuperCalculator/supercalculator/src/main/java/com/jonathan/supercalculator/datamodel/Calculator.java
@@ -1,5 +1,9 @@
 package com.jonathan.supercalculator.datamodel;
 
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Calculator {
 
     private float numberOneFloat;
@@ -9,24 +13,42 @@ public class Calculator {
     }
 
     public String add() {
-        return String.format("%.2f", numberOneFloat + numberTwoFloat);
+        return formatResult(numberOneFloat + numberTwoFloat);
     }
 
     public String subtract() {
-        return String.format("%.2f", numberOneFloat - numberTwoFloat);
+        return formatResult(numberOneFloat - numberTwoFloat);
     }
 
     public String multiply() {
-        return String.format("%.2f", numberOneFloat * numberTwoFloat);
+        return formatResult(numberOneFloat * numberTwoFloat);
     }
 
     public String divide() {
-        return String.format("%.2f", numberOneFloat / numberTwoFloat);
+        return formatResult(numberOneFloat / numberTwoFloat);
     }
 
     public void clear() {
         this.numberOneFloat = 0;
         this.numberTwoFloat = 0;
+    }
+
+    // Helper method for formatting results appropriately.
+    private String formatResult(float result) {
+        // Formats numbers with commas as thousands separators.
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.US);
+
+        // Rounds number up if 5 or above.
+        formatter.setRoundingMode(RoundingMode.HALF_UP);
+
+        // Sets it so only decimal places are shown if required.
+        formatter.setMinimumFractionDigits(0);
+
+        // Sets the maximum number of digits shown after the decimal point to 2.
+        formatter.setMaximumFractionDigits(2);
+
+        // Format the given float result as a String, casting the float to a double.
+        return formatter.format((double) result);
     }
 
     public boolean isValidInput(String numberOne, String numberTwo) {
@@ -43,6 +65,8 @@ public class Calculator {
     public boolean isSecondNumberZero() {
         return numberTwoFloat == 0;
     }
+
+
 
 
 
