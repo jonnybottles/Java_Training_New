b128fc98faf7493c9f984a6672be19ac91a3169c
Jonathan Butler <xxbutler86xx@gmail.com>
2024-02-06 16:23:34 -0500

    ASDFDS

--- csc_600_projects/SuperCalculator/supercalculator/src/main/java/com/jonathan/supercalculator/Controller.java
+++ csc_600_projects/SuperCalculator/supercalculator/src/main/java/com/jonathan/supercalculator/Controller.java
@@ -1,14 +1,104 @@
 package com.jonathan.supercalculator;
 
+import com.jonathan.supercalculator.datamodel.Calculator;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField numberOneField;
+
+    @FXML
+    private TextField numberTwoField;
+
+    @FXML
+    private TextField resultField;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button subtractButton;
+
+    @FXML
+    private Button multiplyButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Label statusLabel;
+
+    private Calculator theCalculator;
+
+    public void initialize() {
+        theCalculator = new Calculator();
     }
+
+    @FXML
+    public void onMathOperationClicked(ActionEvent e) {
+        Button clickedButton = (Button) e.getSource();
+        String buttonText = clickedButton.getText();
+
+        String numberOne = numberOneField.getText().trim();
+        String numberTwo = numberTwoField.getText().trim();
+
+        if (!theCalculator.isValidInput(numberOne, numberTwo)) {
+            displayErrorAlert("Invalid input", "Please enter integer values only.");
+            theCalculator.clear();
+            return;
+        }
+
+        String answer;
+        switch (buttonText) {
+            case "Add":
+                answer = theCalculator.add();
+                break;
+            case "Subtract":
+                answer = theCalculator.subtract();
+                break;
+            case "Multiply":
+                answer = theCalculator.multiply();
+                break;
+            case "Divide":
+                if (theCalculator.isSecondNumberZero()) {
+                    displayErrorAlert("Invalid input", "Cannot divide by zero.");
+                    theCalculator.clear();
+                    return;
+                }
+                theCalculator.divide();
+                break;
+        }
+
+        // NEED TO SET RESULTS FIELD NOW!!!!!!!!!!!!
+
+    }
+
+    public void displayErrorAlert(String header, String msg) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Invalid Input");
+        alert.setHeaderText(header);
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
+
+    public void clearCalculator() {
+        theCalculator.clear();
+        numberOneField.setText("");
+        numberTwoField.setText("");
+        resultField.setText("");
+
+    }
+
 }
--- csc_600_projects/SuperCalculator/supercalculator/src/main/java/com/jonathan/supercalculator/datamodel/Calculator.java
+++ csc_600_projects/SuperCalculator/supercalculator/src/main/java/com/jonathan/supercalculator/datamodel/Calculator.java
@@ -1,4 +1,50 @@
 package com.jonathan.supercalculator.datamodel;
 
 public class Calculator {
+
+    private float numberOneFloat;
+    private float numberTwoFloat;
+
+    public Calculator() {
+    }
+
+    public String add() {
+        return String.format("%.2f", numberOneFloat + numberTwoFloat);
+    }
+
+    public String subtract() {
+        return String.format("%.2f", numberOneFloat - numberTwoFloat);
+    }
+
+    public String multiply() {
+        return String.format("%.2f", numberOneFloat * numberTwoFloat);
+    }
+
+    public String divide() {
+        return String.format("%.2f", numberOneFloat / numberTwoFloat);
+    }
+
+    public void clear() {
+        this.numberOneFloat = 0;
+        this.numberTwoFloat = 0;
+    }
+
+    public boolean isValidInput(String numberOne, String numberTwo) {
+        try {
+            this.numberOneFloat = Float.parseFloat(numberOne);
+            this.numberTwoFloat = Float.parseFloat(numberTwo);
+
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean isSecondNumberZero() {
+        return numberTwoFloat == 0;
+    }
+
+
+
+
 }
