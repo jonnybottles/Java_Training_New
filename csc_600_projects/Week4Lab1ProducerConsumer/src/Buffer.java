6f652f13fd8bd4d16c552f1d1378460164403a14
jbutler <xxbutler86xx@gmail.com>
2024-02-22 18:19:25 -0500

    SFSAD

--- csc_600_projects/Week4Lab1ProducerConsumer/src/Buffer.java
+++ csc_600_projects/Week4Lab1ProducerConsumer/src/Buffer.java
@@ -1,22 +1,29 @@
 import java.util.concurrent.ArrayBlockingQueue;
 
+// Class that represents a packet buffer
 public class Buffer {
-    private ArrayBlockingQueue<Packet> queue;
+    // ArrayBlockingQueue implemented to avoid having to use
+    // wait() / notify() // notifyall(), as the ArrayBlockingQueue
+    // implements this under the hood, ultimately providing synchrinization
+    private ArrayBlockingQueue<Packet> packetQueue;
 
+    // Constructor
     public Buffer(int capacity) {
-        this.queue = new ArrayBlockingQueue<>(capacity);
+        this.packetQueue = new ArrayBlockingQueue<>(capacity);
     }
 
+    // Wrapper method for adding packet to packetQueue
     public void insertPacket(Packet packet) throws InterruptedException {
-        queue.put(packet);
+        packetQueue.put(packet);
     }
 
+    // Wrapper method for removing a packet fro the packetQueue
     public Packet removePacket() throws InterruptedException {
-        return queue.take();
+        return packetQueue.take();
     }
 
     public int getSize() {
-        return queue.size();
+        return packetQueue.size();
     }
 
 
